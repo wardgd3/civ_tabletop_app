9a361d74a7b34001697e020e6018204e3fc50b5d
Grant Ward <wardgd3@gmail.com>
2026-05-08 11:06:28 -0400

    switched from supabase localstorage to sessionstorage

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -3,4 +3,9 @@ import { createClient } from '@supabase/supabase-js'
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_ANON_KEY,
+  {
+    auth: {
+      storage: sessionStorage,
+    },
+  },
 )
